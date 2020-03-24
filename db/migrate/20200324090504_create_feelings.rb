fb46029869d415fc3e73c603ddb9df411802d890
Nishihatak <nishihata1451@gmail.com>
2020-03-24 18:19:44 +0900

    make feelings table

---
+++ db/migrate/20200324090504_create_feelings.rb
@@ -0,0 +1,14 @@
+class CreateFeelings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feelings do |t|
+      t.references :user, foreign_key: true, null: false
+      t.integer    :feel, null: false
+      t.integer    :awake, null: false
+      t.integer    :sleep, null: false
+      t.integer    :tired, null: false
+      t.integer    :body, null: false
+      t.text     :memo
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_083241) do
+ActiveRecord::Schema.define(version: 2020_03_24_090504) do
+
+  create_table "feelings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "feel", null: false
+    t.integer "awake", null: false
+    t.integer "sleep", null: false
+    t.integer "tired", null: false
+    t.integer "body", null: false
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_feelings_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_03_24_083241) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "feelings", "users"
 end
