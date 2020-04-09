3b243153607f2e8e3fff29981ad1cc6990f63994
Nishihatak <nishihata1451@gmail.com>
2020-04-09 16:24:48 +0900

    make a  create_page

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -4,11 +4,27 @@ class AlarmsController < ApplicationController
   end
 
   def new
+    @rate = Rate.new
   end
 
   def create
+    @rate = Rate.create(params_rate)
   end
 
   def show
   end
+
+  private
+
+    def params_rate
+      params.require(:rate).permit(
+        :feel,
+        :sleep,
+        :awake,
+        :tired,
+        :body,
+        :memo
+      ).merge(user_id: current_user.id)
+    end
+  
 end
