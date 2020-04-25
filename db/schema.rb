c0076f207e9e9e0de8a0c2362f3cd56cc45e1794
Nishihatak <nishihata1451@gmail.com>
2020-04-25 19:51:01 +0900

    make alarm_time_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_120101) do
+ActiveRecord::Schema.define(version: 2020_04_25_082237) do
+
+  create_table "alarm_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.time "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_alarm_times_on_user_id"
+  end
 
   create_table "rates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -37,5 +45,6 @@ ActiveRecord::Schema.define(version: 2020_03_24_120101) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "alarm_times", "users"
   add_foreign_key "rates", "users"
 end
---
+++ test/models/alarm_time_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AlarmTimeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
