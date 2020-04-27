4bdde8ab2f860fb47b37ed44bfe3936d4f0a1e0e
Nishihatak <nishihata1451@gmail.com>
2020-04-27 18:21:04 +0900

    alarm_time save DB

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -1,6 +1,12 @@
 class AlarmsController < ApplicationController
 
   def index
+    # @alarm_time = @user.alarm_time
+    unless  @alarm_time
+      @alarm_time = AlarmTime.new
+    end
+      
+
   end
 
   def new
@@ -8,14 +14,25 @@ class AlarmsController < ApplicationController
   end
 
   def create
-    binding.pry
     @rate = Rate.create(params_rate)
+  end
+
+  def update
+    @alarm_time = AlarmTime.create(params_alarm)
   end
 
   def show
   end
 
   private
+
+    def params_alarm
+      params.require(:alarm_time).permit(
+        :time
+      ).merge(user_id: current_user.id)
+
+    end
+
 
     def params_rate
       params.require(:rate).permit(
